84e81aea46ed1bbe895025c0d8bbccd89d9c0688
Juan Pablo Duarte Silva <juan.duarte-s@mail.escuelaing.edu.co>
2026-08-05 10:14:52 -0500

    Actualizacion codigo a ingles en develop

--- Laboratory1/challenge5/Challenge5.java
+++ Laboratory1/challenge5/Challenge5.java
@@ -7,34 +7,33 @@ import java.util.stream.Collectors;
 public class Challenge5 {
 
     // metodo 1 almacena y elimina multiplos de 3
-    public static Set<Integer> filtrarMultiplosDeTres(Set<Integer> entrada) {
-        return entrada.stream()
-                .filter(numero -> numero % 3 != 0)
+    public static Set<Integer> filterMultiplesOfThree(Set<Integer> input) {
+        return input.stream()
+                .filter(number -> number % 3 != 0)
                 .collect(Collectors.toCollection(HashSet::new));
     }
 
     // metodo 2 almacena y elimina multiplos de 5
-    public static Set<Integer> filtrarMultiplosDeCinco(Set<Integer> entrada) {
-        return entrada.stream()
-                .filter(numero -> numero % 5 != 0)
+    public static Set<Integer> filterMultiplesOfFive(Set<Integer> input) {
+        return input.stream()
+                .filter(number -> number % 5 != 0)
                 .collect(Collectors.toCollection(TreeSet::new));
     }
 
     public static void main(String[] args) {
         // Entrada
-        Set<Integer> datosHashSet = new HashSet<>(Arrays.asList(4, 9, 15, 7, 18, 21, 10, 5));
-        Set<Integer> datosTreeSet = new TreeSet<>(Arrays.asList(12, 3, 25, 10, 7, 30, 18, 4));
+        Set<Integer> hashSetData = new HashSet<>(Arrays.asList(4, 9, 15, 7, 18, 21, 10, 5));
+        Set<Integer> treeSetData = new TreeSet<>(Arrays.asList(12, 3, 25, 10, 7, 30, 18, 4));
 
         // filtra
-        Set<Integer> setFiltradoTres = filtrarMultiplosDeTres(datosHashSet);
-        Set<Integer> setFiltradoCinco = filtrarMultiplosDeCinco(datosTreeSet);
+        Set<Integer> filteredSetThree = filterMultiplesOfThree(hashSetData);
+        Set<Integer> filteredSetFive = filterMultiplesOfFive(treeSetData);
 
         // fusiona los resultados en un TreeSet 
-        Set<Integer> arenaFinal = new TreeSet<>(setFiltradoTres);
-        arenaFinal.addAll(setFiltradoCinco);
+        Set<Integer> finalArena = new TreeSet<>(filteredSetThree);
+        finalArena.addAll(filteredSetFive);
 
         // Imprime usando lambda
-        arenaFinal.forEach(numero -> System.out.println("Number in the arena: " + numero));
+        finalArena.forEach(number -> System.out.println("Number in the arena: " + number));
     }
-
-} 
+}
