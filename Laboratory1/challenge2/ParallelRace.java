8f4fca0f800888d91fb363783d92843bd3a65b92
David Felipe Ortiz Salcedo <david.osalcedo@mail.escuelaing.edu.co>
2026-08-05 18:16:00 -0500

    Feature: added second lane code.

--- Laboratory1/challenge2/ParallelRace.java
+++ Laboratory1/challenge2/ParallelRace.java
@@ -1,5 +1,23 @@
+import java.util.*;
+ 
 public class ParallelRace {
+ 
+    public static Integer findMaxNumber(List<Integer> numbers) {
+        return numbers.stream()
+                    .min((a,b) -> a.compareTo(b))
+                    .orElse(0);
+    }
+ 
+    public static Long totalElements(List<Integer> numbers) {
+        return numbers.stream().count();
+    }
     public static void main(String[] args) {
-        
+        List<Integer> listNumbers = Arrays.asList(40, 50, 18, 30, 90, 1000);
+       
+        Integer longNumber = findMaxNumber(listNumbers);
+        Long longList = totalElements(listNumbers);
+       
+        System.out.println("Min number is: " + longNumber);
+        System.out.println("Total number of elements are: " + longList);
     }
 }
