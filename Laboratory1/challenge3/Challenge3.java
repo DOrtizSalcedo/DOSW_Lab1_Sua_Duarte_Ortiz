3a81ffc599b19688ae36535492b325b2783e9d3d
Juan Pablo Duarte Silva <juan.duarte-s@mail.escuelaing.edu.co>
2026-08-05 20:13:34 -0500

    correcion parte con Buffer - solo mensaje al revez

--- Laboratory1/challenge3/Challenge3.java
+++ Laboratory1/challenge3/Challenge3.java
@@ -1,30 +1,20 @@
 import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class Challenge3 {
 
-    // metodo 2 para invertir el mensaje
+    // metodo con StringBuffer
     public static String reverseText(String text) {
         StringBuffer sb = new StringBuffer(text);
         return sb.reverse().toString();
     }
 
-    // ambda para el  Eco
     public static Function<String, String> echoProcess = mensaje -> {
-        String repetido = IntStream.range(0, 3)
-                .mapToObj(i -> mensaje)
-                .collect(Collectors.joining(" "));
-
-        StringBuffer buffer = new StringBuffer(repetido);
+        StringBuffer buffer = new StringBuffer(mensaje);
         return buffer.reverse().toString();
     };
 
     public static void main(String[] args) {
-        System.out.println("Inversiion de la palabra:");
         System.out.println(reverseText("Hola"));
-
-        System.out.println("\nEfecto Eco - Parte 2:");
         System.out.println(echoProcess.apply("Yo"));
     }
 }
