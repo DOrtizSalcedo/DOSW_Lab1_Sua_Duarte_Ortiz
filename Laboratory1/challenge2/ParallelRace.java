28a31076f9b22d223287602ee3827bb4fca9240b
David Felipe Ortiz Salcedo <david.osalcedo@mail.escuelaing.edu.co>
2026-08-05 21:35:36 -0500

    Feature: added final goal.

--- Laboratory1/challenge2/ParallelRace.java
+++ Laboratory1/challenge2/ParallelRace.java
@@ -9,6 +9,15 @@ import java.util.*;
  */
  
 public class ParallelRace {
+
+    public static Results operations(List<Integer> firstList, List<Integer> secondList) {
+        listData first = listMethods(firstList);
+        listData second = listMethods(secondList);
+        
+        ParallelRace result = new ParallelRace();
+        return result.new Results(first, second);
+    }
+
     public static Integer findMinNumber(List<Integer> numbers) {
         return numbers.stream()
                     .min((a,b) -> a.compareTo(b))
@@ -24,40 +33,69 @@ public class ParallelRace {
     public static Long totalElements(List<Integer> numbers) {
         return numbers.stream().count();
     }
-    public static boolean maxValueMultiplier(Integer maxNumber) {
+
+    public static boolean multiplierOrDivisible(Integer maxNumber) {
         return maxNumber % 2 == 0 ? true : false;
     }
 
-    public static boolean maxDivisible(Integer maxNumber) {
-        return maxNumber % 2 == 0 ? true : false;
+    public static String parity(Integer listLength) {
+        return listLength % 2 == 0 ? "even" : "odd";
     }
 
-    public static String listIsEven(Integer length) {
-        return length % 2 == 0 ? "even" : "odd";
+    public static class listData {
+        public int max;
+        public int min;
+        public long size;
+        public boolean maxOrDiv;
+        public String parity;
+
+        public listData(int max, int min, long size, boolean maxOrDiv, String parity) {
+            this.max = max;
+            this.min = min;
+            this.size = size;
+            this.maxOrDiv = maxOrDiv;
+            this.parity = parity;
+        }
+
+        @Override
+        public String toString() {
+            return "Max number: " + max + "\nMin number: " + min +
+                   "\nSize: " + size + "\nIs multiplier or divisor of 2?: " + maxOrDiv + 
+                   "\nParity: " + parity;
+        }
     }
 
-    public static String listIsOdd(Integer listSize) {
-        return listSize % 2 == 0 ? "odd" : "even";
+    public static listData listMethods(List<Integer> numbers) {
+        Integer max = findMaxNumber(numbers);
+        Integer min = findMinNumber(numbers);
+        long listSize = totalElements(numbers);
+        boolean mulOrDiv = multiplierOrDivisible(max);
+        String parityResult = parity((int) listSize);
+
+        return new listData(max, min, listSize, mulOrDiv, parityResult);
+    }
+
+    public class Results {
+        public listData firstResult;
+        public listData secondResult;
+
+        public Results(listData firstResult, listData secondResult) {
+            this.firstResult = firstResult;
+            this.secondResult = secondResult;
+        }
+
+        @Override
+        public String toString() {
+            return "First list:\n" + firstResult + "\nSecond List:\n" + secondResult;
+        }
     }
 
     public static void main(String[] args) {
-        List<Integer> listNumbers = Arrays.asList(40, 50, 18, 30, 90, 1000);
-        Integer listSize = listNumbers.size();
-       
-        Integer longNumber = findMaxNumber(listNumbers);
-        Integer smallNumber = findMinNumber(listNumbers);
-        Long longList = totalElements(listNumbers);
-        Boolean verifyMultiplier = maxValueMultiplier(longNumber);
-        String isEven = listIsEven(listSize);
-        boolean divisible = maxDivisible(longNumber);
-        String isOdd = listIsOdd(listSize);
+        List<Integer> firstListNumbers = Arrays.asList(40, 50, 18, 30, 90, 1000);
+        List<Integer> secondListNumbers = Arrays.asList(60, 37, 89, 5000, 1200);
 
-        System.out.println("Max number is: " + longNumber);
-        System.out.println("Min number is: " + smallNumber);
-        System.out.println("Total number of elements are: " + longList);
-        System.out.println("Is multiplier of 2?: " + verifyMultiplier);
-        System.out.println("The list is: " + isEven);
-        System.out.println("Is divisible by 2?: " + divisible);
-        System.out.println("The list size is: " + isOdd);
+        Results listResults = operations(firstListNumbers, secondListNumbers);
+
+        System.out.println(listResults);
     }
 }
